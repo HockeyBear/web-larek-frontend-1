127838f4650bb587c652265b350220745a1396ce
HockeyBear <nikita.efremov.02@mail.ru>
2024-02-17 21:09:51 +0500

    README.md add & types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,5 +4,28 @@ export interface IProductList {
   image: string,
   title: string,
   category: string,
-  price: number
+  price: number | null,
 }
+
+export interface IAppState {
+  catalog: IProductList[],
+  order: IOrder | null,
+  basket: string[],
+  loading: boolean,
+}
+
+export interface IOrderForm {
+  email: string,
+  phone: string,
+}
+
+export interface IOrderDeliveryForm {
+  payment: string,
+  address: string,
+}
+
+export interface IOrder extends IOrderForm, IOrderDeliveryForm {
+  items: string[],
+}
+
+export type FormError = Partial<Record<keyof IOrder, string>>
